963951e627ce4c2012d7ae1e3fc0d871b0e8f2d6
salmansheri <sheriffsalman00@gmail.com>
2023-03-23 17:23:25 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { logo } from './assets'; 
+
 import { Home, CreatePost } from './pages'; 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
    <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 py-4 px-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src="/img/logotrans.png" alt="logo" className="w-28 object-contain" />
       </Link>
       <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
     
